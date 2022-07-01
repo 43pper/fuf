4be5733de8958ea13df79e1f6c70d8a01981b15c
Stanislav Matsak <stanislav.matsak@nure.ua>
2022-07-01 15:41:01 +0300

    connection

--- src/main/java/com/secag/fuf/FufApplication.java
+++ src/main/java/com/secag/fuf/FufApplication.java
@@ -5,12 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 @RestController
 public class FufApplication {
 
+    Connection connection = null;
     @GetMapping("/message")
     public String message(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://fuf-server.mysql.database.azure.com:3306/fuf?autoReconnect=true", "raexxzgeqi", "Fufadmin!1234");
+            System.out.println(connection);
+        } catch (SQLException e) {
+            System.out.println("ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ");
+        }
         return "Artem siv na velosyped i pishov yistu kabachky";
     }
 
