09cf75a30e3412077a2632ff8e4ba6adbf20072a
Stanislav Matsak <stanislav.matsak@nure.ua>
2022-07-01 15:54:42 +0300

    connection again

--- src/main/java/com/secag/fuf/FufApplication.java
+++ src/main/java/com/secag/fuf/FufApplication.java
@@ -16,13 +16,14 @@ public class FufApplication {
     Connection connection = null;
     @GetMapping("/message")
     public String message(){
+        String mesStart;
         try {
             connection = DriverManager.getConnection("jdbc:mysql://fuf-server.mysql.database.azure.com:3306/fuf?autoReconnect=true", "raexxzgeqi", "Fufadmin!1234");
-            System.out.println(connection);
+            mesStart = connection.toString();
         } catch (SQLException e) {
-            System.out.println("ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ");
+            mesStart = "ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ";
         }
-        return "Artem siv na velosyped i pishov yistu kabachky";
+        return mesStart + "Artem siv na velosyped i pishov yistu kabachky";
     }
 
     public static void main(String[] args) {
