5027c29262c8b9a42c448f12c3e5230e8a8409ef
Stanislav Matsak <stanislav.matsak@nure.ua>
2022-07-04 20:07:10 +0300

    - (exclude = {HibernateJpaAutoConfiguration.class})

--- src/main/java/com/secag/fuf/FufApplication.java
+++ src/main/java/com/secag/fuf/FufApplication.java
@@ -10,7 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @RestController
 public class FufApplication {
 
