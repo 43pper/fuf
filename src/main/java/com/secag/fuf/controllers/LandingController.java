f9b0d5d393be0d420acc0d414808efd5190a0d99
Stanislav Matsak <stanislav.matsak@nure.ua>
2022-07-08 21:16:01 +0300

    +landing -cringe v4

--- src/main/java/com/secag/fuf/controllers/LandingController.java
+++ src/main/java/com/secag/fuf/controllers/LandingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="/")
+@RequestMapping(value="/main")
 public class LandingController {
     @Value("${landing.path}")
     String landingPath;
